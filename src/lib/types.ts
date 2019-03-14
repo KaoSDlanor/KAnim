295b5aba96baf93d88edf81bb5411c476667a1f2
KaoSDlanor <KaoSDlanor@gmail.com>
2019-03-14 20:31:16 +0200

    Changed type of element to HTMLElement from object

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,6 @@ export type milliseconds = number;
 export type unixTimestamp = milliseconds;
 export type percentage = number;
 
-export type element = object;
+export type element = HTMLElement;
 export type propertyKey = string;
 export type propertyValue = number;
